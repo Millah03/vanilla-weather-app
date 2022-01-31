d1daa70b4edd7a2c3ca8cf35fd19959c35bcc3c8
Millah03 <95162665+Millah03@users.noreply.github.com>
2022-01-30 18:01:44 -0800

    Remove console.log

--- src/index.js
+++ src/index.js
@@ -23,8 +23,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
-  console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#city");
